dfc03b3f9cf18a6648e53bbc37d79db6d715dbf4
Pauline Hilborn <philbo130@gmail.com>
2016-03-30 11:07:15 -0400

    adding background image

--- valley_girl_app/config/routes.rb
+++ valley_girl_app/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources   :stores
-  resources   :songs
-  resources   :movies
+	 
+  	resources   :stores
+  	resources   :songs
+  	resources   :movies
+  	root 'landings#index' 
 
 end
 
