931e696cfb26792b0575205c653ad82cdb9102e1
EMBOT415 <embot415@gmail.com>
2016-03-30 14:48:55 -0400

    mp3s working

--- valley_girl_app/app/controllers/songs_controller.rb
+++ valley_girl_app/app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 class SongsController < ApplicationController
-
+	before_action :return_song, only: [:show, :edit, :update, :destroy, :remove_song]
 	def index
+		@test = "this is hello from the songs controller"
+		@songs = Song.all
 
 	end
 
@@ -8,5 +10,22 @@ class SongsController < ApplicationController
 
 	end
 
+	def show
+		@test = "this is hello from the songs controller"
+		@songs = Song.all
 
 
+	end
+
+	# PRIVATE METHODS
+	private
+
+	def return_song
+		@song = Song.find(params[:id])
+	end
+
+	def song_params
+		params.require(:song).permit(:title, :artist, :mp3, :img_url)
+	end
+
+end
