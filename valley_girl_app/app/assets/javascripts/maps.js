168399fdcafe0ace09501c1c920a7305d5da4e7b
EMBOT415 <embot415@gmail.com>
2016-03-31 09:14:23 -0400

    maps up

--- valley_girl_app/app/assets/javascripts/maps.js
+++ valley_girl_app/app/assets/javascripts/maps.js
@@ -1,7 +1,29 @@
-var map;
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+
+//     var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Hello World!'
+//   });
+// }
+
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  var myLatLng = {lat: 34.071489, lng: -118.400564};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
 }
